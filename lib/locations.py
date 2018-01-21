2d5923aba6b7b46cbdb63988c422c321c9d02c5c
Ross Spencer <allalongthewatchtower2001@gmail.com>
2018-01-21 12:45:18 -0600

    Refactoring Palettes
    * Palettes given named tuples
    * Palettes added to own list to be selected automatically
    * Palette name returned with image
    * Palette name returned with Tweet

---
+++ lib/locations.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Local Locs
+wikiloc = "images/FromWikiData.jpg"
+tempfolder = "images"
+paintloc = "PaintedByThePainterGoblin.png"
+listloc = "lib/urilist"
+
+# Pythonanywhere Locs
+'''
+wikiloc = "/home/exponentialdecay/paintergoblin/images/FromWikiData.jpg"
+tempfolder = "/home/exponentialdecay/paintergoblin/images"
+paintloc = "PaintedByThePainterGoblin.png"
+listloc = "/home/exponentialdecay/paintergoblin/lib/urilist"
+'''
